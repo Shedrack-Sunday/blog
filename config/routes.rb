9db09a80828090dc7e8066a090034462d6ad1211
Shedrack-Sunday <hannania002@gmail.com>
2023-01-28 01:31:40 +0100

    Add views form

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  get 'likes/create'
+  get 'comments/create'
   root 'users#index'
+  get 'users/index'
+  get 'users/show'
+  get 'posts/index'
+  get 'posts/show'
   resources :users, only: [:index, :show] do
-  resources :posts, only: [:index, :show]
+  resources :posts, only: [:index, :show, :new, :create]
   end   
-  # get 'users/index'
-  # get 'users/show'
-  # get 'posts/index'
-  # get 'posts/show'
-  
+ 
+
+  resources :posts do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create] 
+  end
 end
